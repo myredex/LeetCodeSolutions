9e104eee6ac4de2ee5bd45315c324411a9b7edad
myredex <ttkz@mail.ru>
2025-07-23 21:04:31 +0500

    Task N5

--- 005_task_347_top-k-frequent-elements_min-heap.py
+++ 005_task_347_top-k-frequent-elements_min-heap.py
@@ -8,34 +8,29 @@ Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
 """
 import heapq
-from typing import *
+from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
         # Counting
-        count = {} # num: freq
+        count = {}
         for n in nums:
-            # Count elements in nums
-            count[n] = count.get(n, 0) + 1
+            count[n] =  count.get(n, 0) + 1
 
-        print(count)
-
+        # Heaping
         heap = []
         for num, freq in count.items():
             heapq.heappush(heap, (freq, num))
-
             if len(heap) > k:
                 heapq.heappop(heap)
 
-        print(heap)
-        res = []
+        # Getting result
+        result = []
         for i in range(k):
-            res.append(heapq.heappop(heap)[1])
+            result.append(heapq.heappop(heap)[1])
 
-        return res
-
-
+        return result
 
 nums = [1,1,1,2,2,3]
 k = 2
