9fda4be00ba04d68e4698e57143960c02638ddab
myredex <ttkz@mail.ru>
2025-08-05 22:18:36 +0500

    Task N11

---
+++ 012_task_150_evaluate_rpn.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+"""
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for t in tokens:
+            if t == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif t == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif t == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif t == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(t))
+        return stack[0]
